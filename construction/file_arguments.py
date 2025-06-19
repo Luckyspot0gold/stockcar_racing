d8a1eac5da5e0c20be614f195a2c1c5333a9d812
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-19 04:11:55 -0600

    Update file_arguments.py

--- construction/file_arguments.py
+++ construction/file_arguments.py
@@ -3,3 +3,8 @@ module.exports = [
   15,                // commissionRate (in %)
   "WyoversePortable" // contractName
 ];
+# Frontier Trader commission flow
+node utils/enableRoyalties.js --rate 7.5%
+
+# Crypto Clashers tournament entry fees
+npx hardhat run scripts/enableTournaments.js
