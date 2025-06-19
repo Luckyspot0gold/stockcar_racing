5902ff5de580222eacd2a629991c8e03c4e406d9
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-19 03:52:46 -0600

    Create hardhat_commissions.js

---
+++ AVAX_P-Chain.js/nft_launch/hardhat_commissions.js
@@ -0,0 +1,12 @@
+# 1. Launch NFTs
+npx hardhat run scripts/launchEmergencySale.js --network avalanche
+
+# 2. Activate commissions
+node utils/enableMarketplaceCommissions.js
+
+# 3. Post Fiverr gig
+OPEN: https://www.fiverr.com/create-gig
+TITLE: "Wyoverse Character Animation Porting - $200/job"
+DESCRIPTION: "I'll convert any Unity/Unreal character to Wyoverse with quantum-ready rigging and Wyoming Protocol 7 compliance"
+TAGS: blockchain, gaming, animation, web3
+PRICE: $200
