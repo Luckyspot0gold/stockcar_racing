4ce3760779f6bcc412a0cb84f5cd734b8bf132a8
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-20 18:23:11 -0600

    Update Server.js
    Signed-off-by: Justin STYRD Gaming <mammerjam303@gmail.com>

--- myUI/Server.js
+++ myUI/Server.js
@@ -12,3 +12,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Crypto Racing Server running on port ${port}`);
 });
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(3000, () => console.log('Racing game running at http://localhost:3000'));
